a4a7900035a9516d5d37783a8500acacfb7c1966
Aakash Pal <aakash.pal@zupee.in>
2021-07-02 14:52:03 +0530

    update browserlist and add lazy imports

--- src/App.js
+++ src/App.js
@@ -1,68 +1,81 @@
+import React, { lazy, Suspense } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { about, contact, home, title } from "./data/hero-texts";
 import "./App.css";
 
-import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import { about, contact, home, title } from "./data/hero-texts";
+const Footer = lazy(() => import("./components/Footer"));
+const HomePage = lazy(() => import("./pages/HomePage"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
+const ContactPage = lazy(() => import("./pages/ContactPage"));
 
 function App() {
-  return (
-    <Router>
-      <Container className="p-0">
-        <Navbar className="border-bottom" bg="trasparent" expand="lg">
-          <Navbar.Brand>
-            <a
-              style={{ textDecoration: "none", color: "inherit" }}
-              href="/"
-            >
-              {title}
-            </a>
-          </Navbar.Brand>
+    return (
+        <Router>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Container className="p-0">
+                    <Navbar
+                        className="border-bottom"
+                        bg="trasparent"
+                        expand="lg"
+                    >
+                        <Navbar.Brand>
+                            <a
+                                style={{
+                                    textDecoration: "none",
+                                    color: "inherit",
+                                }}
+                                href="/"
+                            >
+                                {title}
+                            </a>
+                        </Navbar.Brand>
 
-          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-          <Navbar.Collapse id="navbar-toggle">
-            <Nav className="ml-auto">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+                        <Navbar.Toggle
+                            className="border-0"
+                            aria-controls="navbar-toggle"
+                        />
+                        <Navbar.Collapse id="navbar-toggle">
+                            <Nav className="ml-auto">
+                                <Link className="nav-link" to="/">
+                                    Home
+                                </Link>
+                                <Link className="nav-link" to="/about">
+                                    About
+                                </Link>
+                                <Link className="nav-link" to="/contact">
+                                    Contact
+                                </Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
 
-        <Route
-          path="/about"
-          exact
-          render={() => <AboutPage title={about.title} />}
-        />
-        <Route
-          path="/contact"
-          exact
-          render={() => <ContactPage title={contact.title} />}
-        />
-        <Route
-          path="/"
-          exact
-          render={() => (
-            <HomePage
-              title={home.title}
-              subTitle={home.subTitle}
-              text={home.text}
-            />
-          )}
-        />
-        <Footer />
-      </Container>
-    </Router>
-  );
+                    <Route
+                        path="/about"
+                        exact
+                        render={() => <AboutPage title={about.title} />}
+                    />
+                    <Route
+                        path="/contact"
+                        exact
+                        render={() => <ContactPage title={contact.title} />}
+                    />
+                    <Route
+                        path="/"
+                        exact
+                        render={() => (
+                            <HomePage
+                                title={home.title}
+                                subTitle={home.subTitle}
+                                text={home.text}
+                            />
+                        )}
+                    />
+                    <Footer />
+                </Container>
+            </Suspense>
+        </Router>
+    );
 }
 
 export default App;
